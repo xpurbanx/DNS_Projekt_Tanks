eaae40dd079bf130ee90e16bca278601e597c95c
MikeMG <michalek.mg@gmail.com>
2019-04-07 21:58:57 +0200

    Klasa matczyna: Vehicle (W TRAKCIE)
    tworzenie klasy matczynej dla pojazdów (W.I.P.)

---
+++ DNS_Tanks/Assets/Scripts/Classes/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameClasses
+{
+    public class Vehicle : MonoBehaviour
+    {
+        // PRYWATNE:
+        int hp; // Zdrowie
+        int dmg; // Zadawane obrażenia
+        int spd; // Prędkość pojazdu
+
+        void Movement() {; } // Poruszanie pojazdem
+        void Shoot() {; } // Strzel
+        void PickUp() {; } // Podnieś przedmiot (np. flagę)
+
+        // KONSTRUKTOR I DESTRUKTOR:
+        public Vehicle()
+        {
+            hp = 100;
+            dmg = 0;
+            spd = 50;
+        }
+        
+    }
+}
+
+
