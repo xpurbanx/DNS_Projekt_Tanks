0ea5c7f2a48607447af4c851b175bc5e5592dc4f
Mikolaj Przybylski <clovers02123@gmail.com>
2019-04-12 19:24:44 +0200

    Dostosowanie skryptów do tagów

--- DNS_Tanks/Assets/Scripts/Flag.cs
+++ DNS_Tanks/Assets/Scripts/Flag.cs
@@ -10,33 +10,38 @@ public class Flag : MonoBehaviour
 
     void Start()
     {
-        // Numer flagi jest znany dzięki dwóm ostatnim cyfrom na końcu nazwy obiektu
-        flagNumber = gameObject.name.Substring(gameObject.name.Length - 2);
+        // Numer flagi z jej tagu
+        flagNumber = gameObject.tag.Substring(gameObject.tag.Length - 1);
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        // Pierwsze 6 znaków tagu. Np. z Player 1 będzie to Player
+        string shortTag = other.gameObject.tag.Substring(0, 6);
+
         // Jeżeli z flagą koliduje obiekt o tagu Player
-        if (other.gameObject.tag == "Player")
+        if (shortTag == "Player")
         {
-            // Pobieramy numer tanka, są to dwie ostetnie cyfry nazwy jego obiektu
-            string playerNumber = other.gameObject.name.Substring(gameObject.name.Length - 2);
+            // Pobieramy numer gracza za pomocą tagu (ostatnia cyfra)
+            // Pobieramy gameobject Tank tego gracza, dość brzydko bo za pomocą Gameobject Find, ale nie mogłem wpaść na inny pomysł
+            string playerNumber = other.gameObject.tag.Substring(other.gameObject.tag.Length - 1);
+            GameObject player = GameObject.Find("Tank " + playerNumber);
 
             // Jeżeli czołg, z którym kolidujemy nie jest właścicielem tej flagi (czołg nie może nieść swojej flagi)***
             // I jeżeli czołg, z którym kolidujemy nie posiada już flagi (nie może przecież nieść dwóch na raz)
             // ***Możemy to jeszcze zmienić
-            if (flagNumber != playerNumber && other.GetComponent<PlayerEquipment>().holdingFlag == false)
+            if (flagNumber != playerNumber && player.GetComponent<PlayerEquipment>().holdingFlag == false)
             {
                 // Czołg bierze flagę
-                PickUpFlag(other.gameObject);
+                PickUpFlag(player);
             }
         }
     }
 
-    private void PickUpFlag(GameObject tank)
+    private void PickUpFlag(GameObject player)
     {
         // Zmieniamy w Eq czołgu, że nosi on aktualnie flagę
-        tank.GetComponent<PlayerEquipment>().holdingFlag = true;
+        player.GetComponent<PlayerEquipment>().holdingFlag = true;
         // gameObject flagi ustawiamy na nieaktywny, przez co wszystkie skrypty, collidery i mesh zostają wyłączone
         // Dla podkreślenia: SKRYPTY TEŻ PRZESTAJĄ DZIAŁAĆ
         gameObject.SetActive(false);
