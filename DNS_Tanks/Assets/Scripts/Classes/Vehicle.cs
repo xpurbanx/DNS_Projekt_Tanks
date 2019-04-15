59a7fd328da9aaf34a2157b67447a2fab7d78f23
MikeMG <michalek.mg@gmail.com>
2019-04-15 14:08:46 +0200

    5

---
+++ DNS_Tanks/Assets/Scripts/Classes/Vehicle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour
+{
+    // NIEDZIEDZICZONE
+    int health; // Zdrowie
+    int damage; // Zadawane
+    float speed;
+
+    // PUBLICZNE:
+
+    public float SPD = 3000f;
+
+    public Vehicle()
+    {
+        speed = SPD;
+        health = 100;
+        damage = 0;
+       // spd = 600f;
+    }
+
+    void Update()
+    {
+        PlayerMovement.instance.speed = SPD;
+    }
+}
+
+
+
+
