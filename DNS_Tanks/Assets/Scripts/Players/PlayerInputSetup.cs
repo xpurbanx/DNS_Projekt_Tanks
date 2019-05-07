a5b0e7b073d9bed27a5e0c782e35819a6472fd46
Mikolaj Przybylski <clovers02123@gmail.com>
2019-05-07 11:41:01 +0200

    Update PlayerInputSetup.cs

--- DNS_Tanks/Assets/Scripts/Players/PlayerInputSetup.cs
+++ DNS_Tanks/Assets/Scripts/Players/PlayerInputSetup.cs
@@ -47,8 +47,8 @@ public class PlayerInputSetup : MonoBehaviour
     }
     public bool AButton()
     {
-        //return Input.GetButton(aButton);
-        return false;
+        return Input.GetButton(aButton);
+        //return false;
     }
     public bool BButton()
     {
