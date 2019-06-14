670d6b7c17120a226fe4acba5205bb6da40a93ed
MikeMG <michalek.mg@gmail.com>
2019-06-14 16:01:31 +0200

    OnClick() Progress

--- DNS_Tanks/Assets/Scripts/Respawn.cs
+++ DNS_Tanks/Assets/Scripts/Respawn.cs
@@ -1,19 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("Player1Buttons")]
 
 public class Respawn : MonoBehaviour
 {
+
+
     public List<GameObject> vehicles;
     public string spawnerTag;
     public int startVehicle = 0;
     public int respawnTimer = 2;
-    private bool isSpawning = false;
+    internal bool isSpawning = false;
     Transform spawner;
     Vector3 spawnLocation;
     // Start is called before the first frame update
 
     void Awake()
+    {
+        Launch();
+    }
+
+    internal void Launch()
     {
         spawner = GameObject.FindGameObjectWithTag(spawnerTag).transform;
         spawnLocation = new Vector3(spawner.position.x, spawner.position.y, spawner.position.z);
@@ -22,6 +31,7 @@ public class Respawn : MonoBehaviour
 
     public void SpawnVehicle(int vehicleIndex)
     {
+        Debug.Log(transform.name + "TO TRANSFORM JEST");
         transform.position = spawner.position;
         Instantiate(vehicles[vehicleIndex], spawnLocation, transform.rotation, transform);
         GetComponent<CurrentVehicle>().UpdateCurrentVeh();
@@ -43,7 +53,7 @@ public class Respawn : MonoBehaviour
     }
 
     [ExecuteInEditMode]
-    private void OnDrawGizmos()
+    internal void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawSphere(spawnLocation, 1f);
---
+++ DNS_Tanks/Assets/Scripts/UI/Buttons/Player1Buttons.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Buttons : MonoBehaviour
+{
+    Respawn respawn;
+
+    public void Jeep1()
+    {
+        gameObject.GetComponentInParent<Vehicle>().ForVehicleChooseDestroy();
+        respawn = gameObject.GetComponentInParent<Respawn>();
+        respawn.startVehicle = 1;
+        respawn.Launch();
+    }
+}
