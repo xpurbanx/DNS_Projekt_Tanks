4f7ad5e1977dfdb9fe81b8d5d690adaf30155c12
Purban <xpurbanx@wp.pl>
2019-06-17 19:37:56 +0200

    Zmieniony model czołgu

--- DNS_Tanks/Assets/Scripts/PlayerMovement.cs
+++ DNS_Tanks/Assets/Scripts/PlayerMovement.cs
@@ -68,11 +68,14 @@ public class PlayerMovement : MonoBehaviour
     {
         // Poruszanie się czołgu, jechanie prosto do tyłu i skręcanie
         // Tylko jedna gąsiennica musi dotykać ziemi (?)
-        if (touchingGroundOne || touchingGroundTwo)
-        {
+
+
+        //narazie to wykomentowuje
+      // if (touchingGroundOne || touchingGroundTwo)
+      // {
             Move();
             Turn();
-        }
+       //}
 
         // Dodatkowa grawitacja (?)
         rigidbody.AddForce(-transform.up * 5, ForceMode.Acceleration);
