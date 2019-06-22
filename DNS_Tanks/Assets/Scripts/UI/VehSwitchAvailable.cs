8db644764ec225270a608bc6bf0e8e69592a68ee
MikeMG <michalek.mg@gmail.com>
2019-06-22 16:48:04 +0200

    commit

---
+++ DNS_Tanks/Assets/Scripts/UI/VehSwitchAvailable.cs
@@ -0,0 +1,77 @@
+﻿using System.Runtime.CompilerServices;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[assembly: InternalsVisibleTo("OverlayEnable")]
+[assembly: InternalsVisibleTo("VehicleSwitch")]
+
+public class VehSwitchAvailable : MonoBehaviour
+{
+    public GameObject menu;
+    public double cooldown = 3;
+    internal double timeStamp;
+    private PlayerInputSetup playerInput;
+
+    internal bool menuAvailable;
+    internal bool closeNow;
+    internal bool isOpen = false;
+
+    public void Start()
+    {
+        playerInput = GetComponentInParent<PlayerInputSetup>();
+    }
+
+    private LockActions Lock()
+    {
+        LockActions lockActions = GetComponentInParent<LockActions>();
+        return lockActions;
+    }
+
+    public void OpenMenu()
+    {
+        timeStamp = Time.time + cooldown;
+        if (menu != null && Lock().menusLocked == false && Lock().allLocked == false)
+        {
+
+            Animator animator = menu.GetComponent<Animator>();
+            if (animator != null)
+            {
+                isOpen = animator.GetBool("open");
+                animator.SetBool("open", true);
+                isOpen = true;
+            }
+        }
+    }
+
+    public void CloseMenu()
+    {
+        timeStamp = Time.time + cooldown;
+        if (menu != null && Lock().menusLocked == false && Lock().allLocked == false)
+        {
+
+            Animator animator = menu.GetComponent<Animator>();
+            if (animator != null)
+            {
+                isOpen = animator.GetBool("open");
+                animator.SetBool("open", false);
+                isOpen = false;
+            }
+        }
+    }
+
+    public void SwitchMenu()
+    {
+        timeStamp = Time.time + cooldown;
+        if (menu != null && Lock().menusLocked == false && Lock().allLocked == false)
+        {
+
+            Animator animator = menu.GetComponent<Animator>();
+            if (animator != null)
+            {
+                isOpen = animator.GetBool("open");
+                animator.SetBool("open", !isOpen);
+                isOpen = !isOpen;
+            }
+        }
+    }
+}
