e02b523c0ed6677d861703e9bd85e551accb6c45
MikeMG <michalek.mg@gmail.com>
2019-06-23 18:16:51 +0200

    basics

---
+++ DNS_Tanks/Assets/Scripts/UI/Map.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    int i = 0;
+    public int playerNumber;
+    GameObject[] withPlayerTag;
+    GameObject player; // jeep/tank prefab
+
+    void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        SetUp();
+        if (player != null)
+        {
+            Vector3 newPos = player.transform.position;
+            newPos.y = transform.position.y;
+            transform.position = newPos;
+
+            transform.rotation = Quaternion.Euler(90f, player.transform.eulerAngles.y, 0f);
+        }
+
+    }
+
+    void SetUp()
+    {
+        withPlayerTag = GameObject.FindGameObjectsWithTag("Player " + playerNumber);
+        for (i = 0; i <= withPlayerTag.Length - 1; i++)
+        {
+            if (withPlayerTag[i].GetComponent<PlayerFiring>() && withPlayerTag[i].tag == "Player " + playerNumber)
+                player = withPlayerTag[i];
+        }
+    }
+}
