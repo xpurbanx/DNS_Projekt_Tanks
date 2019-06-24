1cf8b4dc5ce0164feab0eb75b55c5b17466f687e
MikeMG <michalek.mg@gmail.com>
2019-06-24 15:58:42 +0200

    markers ready

--- DNS_Tanks/Assets/Scripts/UI/MinimapTexts.cs
+++ DNS_Tanks/Assets/Scripts/UI/MinimapTexts.cs
@@ -6,13 +6,21 @@ public class MinimapTexts : MonoBehaviour
 {
     GameObject[] texts;
 
-    void Start()
+    void Update()
+    {
+        StartCoroutine(RotateTexts());
+    }
+
+    public IEnumerator RotateTexts()
     {
         texts = GameObject.FindGameObjectsWithTag("Minimap Text");
 
+        yield return new WaitForSecondsRealtime(1 / 60);
+
         for (int i = 0; i <= texts.Length - 1; i++)
         {
-            texts[i].GetComponent<RectTransform>().rotation = Quaternion.Euler(90f, -1.525879e-05f, -1.525879e-05f);
+            if (texts[i] != null)
+                texts[i].GetComponent<RectTransform>().rotation = Quaternion.Euler(90f, -1.525879e-05f, -1.525879e-05f);
         }
     }
 }
