ad7b4a7af018e497519656b235af718f7316ded9
Mikolaj Przybylski <clovers02123@gmail.com>
2019-06-24 20:45:35 +0200

    Blokowanie przycisku po skończeniu się ilości pojazdów

--- DNS_Tanks/Assets/Scripts/LifesManager.cs
+++ DNS_Tanks/Assets/Scripts/LifesManager.cs
@@ -7,19 +7,30 @@ public class LifesManager : MonoBehaviour
     [Header("jeżeli chcesz customizować życia, zostaw atrybut na 0", order = 2)]
     [Space(5, order = 3)]
     public int lifes;
-    public int tankLifes1;
+
     public int jeepLifes1;
+    public int tankLifes1;
     public int heavyTankLifes1;
-    public int tankLifes2;
+
     public int jeepLifes2;
+    public int tankLifes2;
     public int heavyTankLifes2;
 
-    public TextMeshProUGUI jeepsAmount1;
+    public TextMeshProUGUI jeepAmount1;
     public TextMeshProUGUI tankAmount1;
     public TextMeshProUGUI heavyTankAmount1;
-    public TextMeshProUGUI jeepsAmount2;
+
+    public TextMeshProUGUI jeepAmount2;
     public TextMeshProUGUI tankAmount2;
     public TextMeshProUGUI heavyTankAmount2;
+
+    public MyButton jeep1Button;
+    public MyButton tank1Button;
+    public MyButton heavyTank1Button;
+
+    public MyButton jeep2Button;
+    public MyButton tank2Button;
+    public MyButton heavyTank2Button;
 
     private void Start()
     {
@@ -33,10 +44,10 @@ public class LifesManager : MonoBehaviour
             heavyTankLifes2 = lifes;
         }
 
-        jeepsAmount1.text = $"Amount: {jeepLifes1}";
+        jeepAmount1.text = $"Amount: {jeepLifes1}";
         tankAmount1.text = $"Amount: {tankLifes1}";
         heavyTankAmount1.text = $"Amount: {heavyTankLifes1}";
-        jeepsAmount2.text = $"Amount: {jeepLifes2}";
+        jeepAmount2.text = $"Amount: {jeepLifes2}";
         tankAmount2.text = $"Amount: {tankLifes2}";
         heavyTankAmount2.text = $"Amount: {heavyTankLifes2}";
     }
@@ -46,12 +57,24 @@ public class LifesManager : MonoBehaviour
         if (playerNumber == 1)
         {
             tankLifes1 -= 1;
+            if (tankLifes1 == 0)
+            {
+                tankAmount1.color = new Color(0.885f, 0.44f, 0.35f);
+                tank1Button.interactable = false;
+            }
+
             tankAmount1.text = $"Amount: {tankLifes1}";
         }
 
         else
         {
             tankLifes2 -= 1;
+            if (tankLifes2 == 0)
+            {
+                tankAmount2.color = new Color(0.885f, 0.44f, 0.35f);
+                tank2Button.interactable = false;
+            }
+
             tankAmount2.text = $"Amount: {tankLifes2}";
         }
 
@@ -63,13 +86,25 @@ public class LifesManager : MonoBehaviour
         if (playerNumber == 1)
         {
             jeepLifes1 -= 1;
-            jeepsAmount1.text = $"Amount: {jeepLifes1}";
+            if (jeepLifes1 == 0)
+            {
+                jeepAmount1.color = new Color(0.885f, 0.44f, 0.35f);
+                jeep1Button.interactable = false;
+            }
+
+            jeepAmount1.text = $"Amount: {jeepLifes1}";
         }
 
         else
         {
             jeepLifes2 -= 1;
-            jeepsAmount2.text = $"Amount: {jeepLifes2}";
+            if (jeepLifes2 == 0)
+            {
+                jeepAmount2.color = new Color(0.885f, 0.44f, 0.35f);
+                jeep2Button.interactable = false;
+            }
+
+            jeepAmount2.text = $"Amount: {jeepLifes2}";
         }
 
         CheckIfGameOver();
@@ -80,12 +115,24 @@ public class LifesManager : MonoBehaviour
         if (playerNumber == 1)
         {
             heavyTankLifes1 -= 1;
+            if (heavyTankLifes1 == 0)
+            {
+                heavyTankAmount1.color = new Color(0.885f, 0.44f, 0.35f);
+                heavyTank1Button.interactable = false;
+            }
+
             heavyTankAmount1.text = $"Amount: {heavyTankLifes1}";
         }
 
         else
         {
             heavyTankLifes2 -= 1;
+            if (heavyTankLifes2 == 0)
+            {
+                heavyTankAmount2.color = new Color(0.885f, 0.44f, 0.35f);
+                heavyTank2Button.interactable = false;
+            }
+
             heavyTankAmount2.text = $"Amount: {heavyTankLifes2}";
         }
 
