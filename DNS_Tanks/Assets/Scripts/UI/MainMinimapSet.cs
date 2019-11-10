fcafd97de36dc47650a151cf8879f3d1e30760be
MikeMG <michalek.mg@gmail.com>
2019-11-10 16:01:27 +0100

    Work

---
+++ DNS_Tanks/Assets/Scripts/UI/MainMinimapSet.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMinimapSet : MonoBehaviour
+{
+    // FUNKCJA USTAWIAJĄCA PARAMETRY MINIMAPY DLA OKREŚLONEGO POZIOMU
+    void SetCam(string sceneName)
+    {
+        Vector3 camPos = Vector3.zero;
+        float cameraSize = 0f;
+        switch (sceneName)
+        {
+            case "Desert Battle":
+                camPos = new Vector3(485f, 70.32f, 1359f);
+                cameraSize = 227.6f;
+
+                transform.localPosition = camPos;
+                transform.GetComponent<Camera>().orthographicSize = cameraSize;
+                return;
+
+            case "Urantia":
+                camPos = new Vector3(485f, 70.32f, 1359f);
+                cameraSize = 227.6f;
+
+                transform.localPosition = camPos;
+                transform.GetComponent<Camera>().orthographicSize = cameraSize;
+                return;
+
+            default:
+                return;
+        }
+    }
+
+
+
+    // ROZPOCZĘCIE ŚLEDZENIA WCZYTYWANYCH POZIOMÓW
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+
+
+    // WYWOŁANIE FUNKCJI USTAWIAJĄCEJ PARAMETRY MINIMAPY 
+    void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        SetCam(scene.name);
+    }
+
+
+
+    // ZAKOŃCZENIE ŚLEDZENIA WCZYTYWANYCH POZIOMÓW
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+}
