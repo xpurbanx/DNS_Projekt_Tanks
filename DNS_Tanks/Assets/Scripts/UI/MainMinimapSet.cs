70c75c3515def6ee237e08b125f4d6992af4e9a7
MikeMG <michalek.mg@gmail.com>
2019-11-18 18:12:43 +0100

    tweaks & fixes

--- DNS_Tanks/Assets/Scripts/UI/MainMinimapSet.cs
+++ DNS_Tanks/Assets/Scripts/UI/MainMinimapSet.cs
@@ -5,26 +5,55 @@ using UnityEngine;
 
 public class MainMinimapSet : MonoBehaviour
 {
-    // FUNKCJA USTAWIAJĄCA PARAMETRY MINIMAPY DLA OKREŚLONEGO POZIOMU
+    // FUNKCJA USTAWIAJĄCA PARAMETRY MINIMAPY DLA OKREŚLONEGO POZIOMU ORAZ MAP ROZWIJANYCH OBU GRACZY
     void SetCam(string sceneName)
     {
+        GameObject[] M1, M2;
+        GameObject minimap;
         Vector3 camPos = Vector3.zero;
+        Vector3 camRot = Vector3.zero;
         float cameraSize = 0f;
+
+
+
+        M1 = GameObject.FindGameObjectsWithTag("Minimap 1");
+        M2 = GameObject.FindGameObjectsWithTag("Minimap 2");
+        minimap = GameObject.FindGameObjectWithTag("MainMinimapCam");
+
+
+
+        for (int i = 0; i <= M1.Length - 1; i++)
+        {
+            if (M1[i] != null)
+                M1[i].transform.localEulerAngles = new Vector3(90f, minimap.transform.rotation.eulerAngles.y, 0f);
+        }
+        for (int i = 0; i <= M2.Length - 1; i++)
+        {
+            if (M2[i] != null)
+                M2[i].transform.localEulerAngles = new Vector3(90f, minimap.transform.rotation.eulerAngles.y, 0f);
+        }
+
+
+
         switch (sceneName)
         {
             case "Desert Battle":
                 camPos = new Vector3(485f, 70.32f, 1359f);
-                cameraSize = 227.6f;
+                camRot = new Vector3(90f, 0f, 0f);
+                cameraSize = 247.6f;
 
                 transform.localPosition = camPos;
+                transform.localEulerAngles = camRot;
                 transform.GetComponent<Camera>().orthographicSize = cameraSize;
                 return;
 
             case "Urantia":
-                camPos = new Vector3(485f, 70.32f, 1359f);
-                cameraSize = 227.6f;
+                camPos = new Vector3(249f, 70.32f, 784f);
+                camRot = new Vector3(90f, -90f, 0f);
+                cameraSize = 165.87f;
 
                 transform.localPosition = camPos;
+                transform.localEulerAngles = camRot;
                 transform.GetComponent<Camera>().orthographicSize = cameraSize;
                 return;
 
@@ -43,7 +72,7 @@ public class MainMinimapSet : MonoBehaviour
 
 
 
-    // WYWOŁANIE FUNKCJI USTAWIAJĄCEJ PARAMETRY MINIMAPY 
+    // WYWOŁANIE FUNKCJI USTAWIAJĄCEJ PARAMETRY MINIMAP
     void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
     {
         SetCam(scene.name);
--- DNS_Tanks/Assets/Scripts/UI/MinimapTexts.cs
+++ DNS_Tanks/Assets/Scripts/UI/MinimapTexts.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MinimapTexts : MonoBehaviour
 {
     GameObject[] texts;
+    GameObject minimap;
 
     void Start()
     {
@@ -14,13 +15,14 @@ public class MinimapTexts : MonoBehaviour
     public IEnumerator RotateTexts()
     {
         texts = GameObject.FindGameObjectsWithTag("Marker");
+        minimap = GameObject.FindGameObjectWithTag("MainMinimapCam");
 
         yield return new WaitForSecondsRealtime(1 / 60);
 
         for (int i = 0; i <= texts.Length - 1; i++)
         {
-            if (texts[i] != null)
-                texts[i].GetComponent<Transform>().rotation = Quaternion.Euler(90f, -1.525879e-05f, -1.525879e-05f);
+            if (texts[i] != null && minimap != null)
+                texts[i].GetComponent<Transform>().rotation = Quaternion.Euler(90f, minimap.transform.rotation.eulerAngles.y, 0f);//-1.525879e-05f, -1.525879e-05f);
         }
     }
 }
