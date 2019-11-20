56e403404049af71930dcd4aeb4dff0a3d2484de
MikeMG <michalek.mg@gmail.com>
2019-11-20 06:39:47 +0100

    small fixes

--- DNS_Tanks/Assets/Scripts/UI/MinimapTexts.cs
+++ DNS_Tanks/Assets/Scripts/UI/MinimapTexts.cs
@@ -6,31 +6,22 @@ public class MinimapTexts : MonoBehaviour
 {
     GameObject[] texts;
     GameObject[] mainMapMarkers;
+    GameObject[] playerMainMarkers;
     GameObject minimap;
 
     void Start()
     {
-        StartCoroutine(RotateTexts());
+        //StartCoroutine(RotateTexts());
     }
 
     private void Update()
     {
-        mainMapMarkers = GameObject.FindGameObjectsWithTag("MainMapMarkers");
-        for (int i = 0; i <= mainMapMarkers.Length - 1; i++)
-        {
-            mainMapMarkers[i].transform.localScale = new Vector3(minimap.GetComponent<Camera>().orthographicSize * 0.08481f, minimap.GetComponent<Camera>().orthographicSize * 0.08481f, 0f);
-            Debug.Log(mainMapMarkers[i].transform.localScale + " <---");
-        }
-    }
-
-    public IEnumerator RotateTexts()
-    {
-
         texts = GameObject.FindGameObjectsWithTag("Marker");
         mainMapMarkers = GameObject.FindGameObjectsWithTag("MainMapMarkers");
         minimap = GameObject.FindGameObjectWithTag("MainMinimapCam");
+        playerMainMarkers = GameObject.FindGameObjectsWithTag("PlayerMainMarkers");
 
-        yield return new WaitForSecondsRealtime(1 / 60);
+        //yield return new WaitForSecondsRealtime(1 / 60);
 
         for (int i = 0; i <= texts.Length - 1; i++)
         {
@@ -50,6 +41,32 @@ public class MinimapTexts : MonoBehaviour
                 mainMapMarkers[i].GetComponent<Transform>().rotation = Quaternion.Euler(90f, minimap.transform.rotation.eulerAngles.y, 0f);//-1.525879e-05f, -1.525879e-05f);
         }
 
+        mainMapMarkers = GameObject.FindGameObjectsWithTag("MainMapMarkers");
+        for (int i = 0; i <= mainMapMarkers.Length - 1; i++)
+        {
+            mainMapMarkers[i].transform.localScale = new Vector3(minimap.GetComponent<Camera>().orthographicSize * 0.08481f, minimap.GetComponent<Camera>().orthographicSize * 0.08481f, 0f);
+            Debug.Log(mainMapMarkers[i].transform.localScale + " <---");
+        }
+
+        playerMainMarkers = GameObject.FindGameObjectsWithTag("PlayerMainMarkers");
+        if (playerMainMarkers != null)
+        {
+            for (int i = 0; i <= playerMainMarkers.Length - 1; i++)
+            {
+                playerMainMarkers[i].transform.localScale = new Vector3(minimap.GetComponent<Camera>().orthographicSize * 0.08481f, minimap.GetComponent<Camera>().orthographicSize * 0.08481f, 0f);
+                Debug.Log(playerMainMarkers[i].transform.localScale + " <---");
+            }
+        }
+
+
 
     }
+
+    /*public IEnumerator RotateTexts()
+    {
+
+
+        
+
+    }*/
 }
